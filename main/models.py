b57acb21815f48e4a949cd0c44f09b8e2b709f92
alya rasheeda <alyarasheeda@alyas-MacBook-Air.local>
2024-09-11 10:41:31 +0700

    six commit

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class MoodEntry(models.Model):
+    nama = models.CharField(max_length=200)
+    kelas = models.IntegerField()
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
--- main/views.py
+++ main/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
+        'nama' : 'Alya Rasheeda Yuvana',
+        'kelas' : 'PBP B',
         'name' : 'Blazer',
         'price': '2000000',
         'description': 'Pink'
