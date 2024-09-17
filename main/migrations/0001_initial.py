9eb355e22ca797acc2763d8fe2ae7afd4ef0caed
alya rasheeda <alyarasheeda@alyas-MacBook-Air.local>
2024-09-17 22:57:51 +0700

    test

--- main/migrations/0001_initial.py
+++ main/migrations/0001_initial.py
@@ -14,7 +14,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='MoodEntry',
             fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=255)),
+                # ('time', models.DateField(auto_now_add=True)),
                 ('price', models.IntegerField()),
                 ('description', models.TextField()),    
             ],
--- shop_to_impress/settings.py
+++ shop_to_impress/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'shop_to_impress.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
